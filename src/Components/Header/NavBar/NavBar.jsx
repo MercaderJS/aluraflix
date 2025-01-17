f3fcf5547d57b102327e1c987e6bd5bc58f970a5
EnmanuelJS <galvanenmanuel0@gmail.com>
2025-01-17 13:17:44 -0400

    creando funcion de redireccionamiento boton Home

--- src/Components/Header/NavBar/NavBar.jsx
+++ src/Components/Header/NavBar/NavBar.jsx
@@ -1,13 +1,16 @@
 import { useState } from "react";
 import "./NavBar.css";
 
-function NavBar() {
+function NavBar(props) {
+
+    const BotonHome = () => window.location.href='/';
+
     return (
         <nav className="nav__container">
             <img src="img/image 1.png" alt="logo aluraflix" />
 
             <div className="button__container">
-            <button className="home__button">Home</button>
+            <button className="home__button" onClick={BotonHome}>Home</button>
             <button className="new__video--button">Nuevo Video</button>
             </div>
 
